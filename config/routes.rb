e6233a3f48d510f259bb957cd3359a70aa45497d
lampv <lampvhy@gmail.com>
2022-05-11 01:55:46 +0900

    add signup and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "home/index"
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
   root to: "home#index"
 end
