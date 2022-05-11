0330a4092c626477a165c37c9a92d66b06f40f12
lampv <lampvhy@gmail.com>
2022-05-11 14:43:40 +0900

    fix css

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,8 +3,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def current_user
-    @current_user ||= User.find_by(id: user_id)
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def current_user?(user)
