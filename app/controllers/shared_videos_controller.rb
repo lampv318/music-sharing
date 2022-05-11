41193d7f633a489c28feb3b14306c38cfff9ea13
lampv <lampvhy@gmail.com>
2022-05-12 01:09:15 +0900

    remove reduntdant feature

--- app/controllers/shared_videos_controller.rb
+++ app/controllers/shared_videos_controller.rb
@@ -1,4 +1,6 @@
 class SharedVideosController < ApplicationController
+  before_action :logged_in_user, only: [:create]
+
   def index
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,12 +3,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
@@ -24,14 +18,5 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
-  end
-
-  def redirect_back_or(default)
-    redirect_to(session[:forwarding_url] || default)
-    session.delete(:forwarding_url)
-  end
-
-  def store_location
-    session[:forwarding_url] = request.original_url if request.get?
   end
 end
