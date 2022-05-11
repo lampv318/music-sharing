ad597465cc1ee915da23af8550e206b73f5813a0
lampv <lampvhy@gmail.com>
2022-05-12 00:27:04 +0900

    add test user model

--- Gemfile
+++ Gemfile
@@ -75,6 +75,7 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem 'minitest'
 end
 
 group :production do
--- test/models/shared_video_test.rb
+++ test/models/shared_video_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class SharedVideoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @share_video = SharedVideo.new(url: 'https://www.youtube.com/watch?v=f9WIBYwLnb0', user_id: users(:michael).id)
+  end
+
+  test "should be valid" do
+    assert @share_video.valid?
+  end
+
+  test "url should be present" do
+    @share_video.url = "     "
+    assert_not @share_video.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
 end
