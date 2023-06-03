e2af3bbb4a6a4de30e62100a298d36efe4c9f780
lampv <lampvhy@gmail.com>
2023-06-03 14:45:21 +0700

    add login logout page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "home/index"
   root to: "home#index"
+
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
 end
