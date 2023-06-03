d92f56e9c938087ac1f51ce7c7bd92ab3ef384c4
lampv <lampvhy@gmail.com>
2023-06-03 15:53:09 +0700

    add shared video form

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,15 +10,7 @@ module SessionsHelper
   end
 
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(:remember, cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def current_user?(user)
@@ -27,12 +19,6 @@ module SessionsHelper
 
   def logged_in?
     !current_user.nil?
-  end
-
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
   end
 
   def log_out
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :shared_videos, only: %i( index new create )
 end
